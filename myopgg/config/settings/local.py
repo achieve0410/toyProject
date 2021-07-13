3f5bcd7cbd5e41b12b01349bee7fb34e3b50211d
wonhyo.choi <pray@kakao.com>
2021-07-14 03:01:53 +0900

    change allowhost

--- myopgg/config/settings/local.py
+++ myopgg/config/settings/local.py
@@ -11,7 +11,8 @@ SECRET_KEY = env(
     default="YZGmrkgDTEMDsVamwxNWyC54TDSHdg5sng15BZNGHylubGmyUFKYomz41OfQUSYJ",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+# ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
